a09b02bc18ac1a29fb8222836346ae2314f3155b
Alan <king.python@mail.ru>
2024-09-18 20:35:00 +0300

    Переключение

--- src/components/Pages/AdminPanel/AdminPanel.tsx
+++ src/components/Pages/AdminPanel/AdminPanel.tsx
@@ -11,28 +11,28 @@ import { div } from 'framer-motion/client';
 const AdminPanel: React.FC = () => {
   return (
 
-    <div className='grid grid-cols-6 grid-rows-6 w-full h-full gap-[20px]'>
-      <div className=' bg-slate-600 col-[1/3] row-[1]'></div>
+    // <div className='grid grid-cols-6 grid-rows-6 w-full h-full gap-[20px]'>
+    //   <div className=' bg-slate-600 col-[1/3] row-[1]'></div>
 
-    </div>
-
-
-
-
-    // <div className="flex h-screen bg-gray-100">
-    //   <Sidebar />
-    //   <div className="flex-1 flex flex-col overflow-hidden">
-    //     <Header />
-    //     <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200 p-4">
-    //       <Routes>
-    //         <Route path="admin/map" element={<MapComponent />} />
-    //         <Route path="users" element={<Users />} />
-    //         <Route path="settings" element={<Settings />} />
-    //         <Route path="reports" element={<Reports />} />
-    //       </Routes>
-    //     </main>
-    //   </div>
     // </div>
+
+
+
+
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar />
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200 p-4">
+          <Routes>
+            <Route path="admin/map" element={<MapComponent />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="reports" element={<Reports />} />
+          </Routes>
+        </main>
+      </div>
+    </div>
   );
 };
 
